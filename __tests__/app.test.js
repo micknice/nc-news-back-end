217db0d283e7e855dfa4b31d8f46bbc9fd3b6c39
mick nice <mikemccullagh@outlook.com>
2023-02-21 00:26:00 +0000

    added articles controller, endpoint, controller function, model function and tests for task 4 get/api/articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,7 +20,7 @@ describe('api', () => {
 
 })
 describe('GET /api/topics', () => {
-    test('get  /api/topics request reponds with topics object containing an array of topic objects ', () => {
+    test('get  /api/topics request responds with topics object containing an array of topic objects ', () => {
         return request(app)
         .get('/api/topics')
         .expect(200)
@@ -34,6 +34,43 @@ describe('GET /api/topics', () => {
                         })       
             });
 
+        })
+    })
+})
+describe('GET /api/articles', () => {
+    test('get /api/articles request responds with articles object containing an array of articles objects', () => {
+        return request(app)
+        .get('/api/articles')
+        .expect(200)
+        .then(response => {
+            const resBodyArticles = response.body.articles;
+            expect(resBodyArticles.length).toBe(12)
+            resBodyArticles.forEach(article => {
+                console.log(typeof article.created_at)
+                expect(article).toMatchObject({
+                    author: expect.any(String),
+                    title: expect.any(String),
+                    article_id: expect.any(Number),
+                    topic: expect.any(String),
+                    created_at: expect.any(String),
+                    votes: expect.any(Number),
+                    comment_count: expect.any(Number)
+
+                })
+            })
+        })
+    })
+    test('get /api/articles request returns array sorted by date created in ascending order', () => {
+        return request(app)
+        .get('/api/articles')
+        .expect(200)
+        .then(response => {
+            const datesArray = response.body.articles.map(x => x.created_at = parseInt(x.created_at.slice(0, 9).replace(/-/g, '')));
+            console.log(datesArray)
+            for (let i = 0; i < datesArray.length - 1; i++) {
+                expect(typeof datesArray[i]).toBe('number')
+                expect(datesArray[i]).toBeLessThanOrEqual(datesArray[i + 1])
+            }           
         })
     })
 })
